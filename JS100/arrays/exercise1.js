cda7df58501d71e73ca53eb1dbbdc7da0491f5f5
root <root@DESKTOP-2JGG68N.localdomain>
2020-05-09 11:19:55 -0600

    modified solution text position to align with the statements in a more meaningful posiition

--- JS100/arrays/exercise1.js
+++ JS100/arrays/exercise1.js
@@ -1,14 +1,14 @@
 let array1 = [1, 2, undefined, 4];  // length is 3  <--incorrect
                                     // length is 4  <--correct //Length is equal to the highest index position that has value, plus 1
 
-let array2 = [1];                   // length is 5
-array2.length = 5;
+let array2 = [1];                  
+array2.length = 5;                  // length is 5
 
-let array3 = [];                    // length is 0
-array3[-1] = [1];
+let array3 = [];                    
+array3[-1] = [1];                   // length is 0
 
-let array4 = [1, 2, 3, 4, 5];       // length is 3
-array4.length = 3;
+let array4 = [1, 2, 3, 4, 5];       
+array4.length = 3;                  // length is 3
 
-let array5 = [];                    // length is 101
-array5[100] = 3;
+let array5 = [];                    
+array5[100] = 3;                    // length is 101
